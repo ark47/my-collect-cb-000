cec97600cfb7d8ef12e393684dda8ce5adcee4d1
Chris Nunez <cenunez47@gmail.com>
2017-09-20 00:04:29 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array)
+  x = 0
+  arr = []
+  while x < array.length
+    arr << yield(array[x])
+    arr += 1
+  end
+  arr
+end
 
-
+# def hello(array)
+#   i = 0
+#   collection = []
+#   while i < array.length
+#     collection << yield(array[i])
+#     i += 1
+#   end
+#
+#   collection
+#
+# end
+#
+# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
