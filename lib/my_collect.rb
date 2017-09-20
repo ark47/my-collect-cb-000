66a3ca88f50f3bd630c8d8732cb39f7897d3cfce
Chris Nunez <cenunez47@gmail.com>
2017-09-20 00:05:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,21 +1,21 @@
-def my_collect(array)
-  x = 0
-  arr = []
-  while x < array.length
-    arr << yield(array[x])
-    arr += 1
-  end
-  arr
-end
-
 # def my_collect(array)
-#   i = 0
-#   collection = []
-#   while i < array.length
-#     collection << yield(array[i])
-#     i += 1
+#   x = 0
+#   arr = []
+#   while x < array.length
+#     arr << yield(array[x])
+#     arr += 1
 #   end
-#
-#   collection
-#
+#   arr
 # end
+
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+
+  collection
+
+end
